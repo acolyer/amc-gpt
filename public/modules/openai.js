036380450155d5e930b38fc359d384527608b8e6
Adrian Colyer <adrian@aspectprogrammer.org>
2023-04-25 09:15:06 +0100

    Add openai API module

---
+++ public/modules/openai.js
@@ -0,0 +1,35 @@
+import { GPT_35_TURBO } from "./model_names.js";
+
+const DEFAULT_COMPLETION_MODEL = GPT_35_TURBO;
+const OPENAI_CHAT_COMPLETION_ENDPOINT = 'https://api.openai.com/v1/chat/completions';
+const OPENAI_IMAGE_GENERATION_ENDPOINT = 'https://api.openai.com/v1/images/generations';
+
+async function getChatCompletion(apiKey, messages, model=DEFAULT_COMPLETION_MODEL) {
+    return await fetch(OPENAI_CHAT_COMPLETION_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + apiKey
+        },
+        body: JSON.stringify({
+            'model': model,
+            'messages': messages
+        })
+    });
+}
+
+async function getImage(apiKey, imageSpec) {
+    return await fetch(OPENAI_IMAGE_GENERATION_ENDPOINT, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + apiKey
+        },
+        body: JSON.stringify({
+            'prompt': imageSpec,
+            'size': '512x512'
+        })
+    });
+}
+
+export { getChatCompletion, getImage }
---
+++ tests/openai.spec.js
@@ -0,0 +1,72 @@
+import { getChatCompletion, getImage } from "../public/modules/openai.js";
+import fetchMocks from 'jest-fetch-mock';
+
+fetchMocks.enableFetchMocks();
+
+beforeEach(() => {
+    fetchMocks.resetMocks();
+})
+
+describe('getChatCompletion', () => {
+    it('should POST to the OpenAI chat/completions endpoint', () => {
+        fetchMocks.mockResponseOnce({status: 200});
+        const result = getChatCompletion('api-key',[{role: 'user', content: 'A message'}]);
+        const apiCall = fetch.mock.calls[0];
+        const fetchedURL = apiCall[0];
+        expect(fetchedURL).toEqual('https://api.openai.com/v1/chat/completions');
+        const callParameters = apiCall[1];
+        expect(callParameters.method).toEqual('POST');
+    });
+
+    it('should create a bearer token using the api key', () => {
+        fetchMocks.mockResponseOnce({status: 200});
+        const result = getChatCompletion('api-key',[{role: 'user', content: 'A message'}]);
+        const apiCall = fetch.mock.calls[0];
+        const callParameters = apiCall[1];
+        expect(callParameters.headers['Authorization']).toEqual('Bearer api-key');
+    });
+
+    it('should include the model and messages parameters in the body', () => {
+        fetchMocks.mockResponseOnce({status: 200});
+        const result = getChatCompletion('api-key',[{role: 'user', content: 'A message'}]);
+        const apiCall = fetch.mock.calls[0];
+        const callParameters = apiCall[1];
+        const expectedBody = JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: 'A message' }]
+        });
+        expect(callParameters.body).toEqual(expectedBody);
+    });
+});
+
+describe('getImage', () => {
+    it('should POST to the OpenAI image generation endpoint', () => {
+        fetchMocks.mockResponseOnce({status: 200});
+        const result = getImage('api-key', 'a picture of a giraffe');
+        const apiCall = fetch.mock.calls[0];
+        const fetchedURL = apiCall[0];
+        expect(fetchedURL).toEqual('https://api.openai.com/v1/images/generations');
+        const callParameters = apiCall[1];
+        expect(callParameters.method).toEqual('POST');        
+    });
+
+    it('should create a bearer token using the api key', () => {
+        fetchMocks.mockResponseOnce({status: 200});
+        const result = getImage('api-key', 'a picture of a giraffe');
+        const apiCall = fetch.mock.calls[0];
+        const callParameters = apiCall[1];
+        expect(callParameters.headers['Authorization']).toEqual('Bearer api-key');
+    });    
+
+    it('should request a 512x512 image with the given spec', () => {
+        fetchMocks.mockResponseOnce({status: 200});
+        const result = getImage('api-key', 'a picture of a giraffe');
+        const apiCall = fetch.mock.calls[0];
+        const callParameters = apiCall[1];
+        const expectedBody = JSON.stringify({
+            prompt: 'a picture of a giraffe',
+            size: '512x512'
+        });
+        expect(callParameters.body).toEqual(expectedBody);
+    });
+});
