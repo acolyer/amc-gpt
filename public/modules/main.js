a0042ff47262d260aab6ab7ae8101075ca0c565a
Adrian Colyer <adrian@aspectprogrammer.org>
2023-05-06 08:51:26 +0100

    Refactoring

--- public/modules/main.js
+++ public/modules/main.js
@@ -11,55 +11,84 @@ import { initAnkiExtension } from "./showdown_anki.js";
 import { SectionParser } from "./parser.js";
 
 console.log('amc-gpt starting');
-showdowns.init();
-initImagesExtension();
-initAnkiExtension();
-initBindings();
 logSystemMessage(getSystemPrompt()); 
-var sectionParser = null;
 
+initialiseShowdowns();
+initBindings();
 
+// register a handler for when the user sends a message, then sit back and wait!
 onSendMessage(async (event) => {
-   event.preventDefault();
-   clearSystemMessages();
-   clearGPTOutputs();
+   event.preventDefault();  
+   if (!checkForApiKey()) { return }
 
    const message = getUserInput();
-   showUserChatMessage( message);
-   showThinking();
+   prepareUIForNewMessageExchange(message);
 
+   // the user prompt may contain additional system instructions alongside the
+   // message the user entered
    const userPrompt = getUserPrompt(message);
    logUserMessage(userPrompt);
 
+   sendMessageAndStreamResponse();
+});
+
+/**
+ * The first chunk of the streaming response has arrived, we can expect
+ * content to be flowing in from this point onwards.
+ */
+function contentStart() {
+   hideThinking();
+   startTyping();
+}
+
+/**
+ * The last chunk of the streaming response has arrived, and hence there
+ * will be no more new content for this reply. 
+ */
+function contentEnd() {
+   stopTyping();
+   logAssistantMessage(sectionParser.getRawContent());
+   rememberContent();
+   enableSending();
+}
+
+function initialiseShowdowns() { 
+   showdowns.init();
+   initImagesExtension();
+   initAnkiExtension();
+}
+
+function checkForApiKey() {
    const apiKey = getApiKey();
    if (apiKey == undefined || apiKey.length == 0) {
         showSystemMessage("API key is required");
-        hideThinking();
-        return;
+        return false;
    } 
+   return true;
+}
 
+function prepareUIForNewMessageExchange(userMessage) {
+   // remove any content relating to the previous exchange
+   clearSystemMessages();
+   clearGPTOutputs();
+
+   showUserChatMessage(userMessage);
+   showThinking();
+
+   // block concurrent requests, and create a placeholder for the reply
    disableSending();
    createAssistantChatMessage();
+}
+
+async function sendMessageAndStreamResponse() {
    const response = getChatCompletion(getApiKey(), getChatMessages());
    const data = await response;
    if (data.status == 200) {
-      sectionParser = new SectionParser(sectionConfig, contentStart, contentEnd);
+      const sectionParser = new SectionParser(sectionConfig, contentStart, contentEnd);
       getStreamedContent(data, content => sectionParser.processChunk(content));
    } else {
       showSystemMessage(`Error returned from OpenAI API, status code: ${data.status}`);
       stopTyping();
       enableSending();
    }
-});
-
-function contentStart() {
-   hideThinking();
-   startTyping();
-}
-
-function contentEnd() {
-   stopTyping();
-   logAssistantMessage(sectionParser.getRawContent());
-   rememberContent();
-   enableSending();
 }
